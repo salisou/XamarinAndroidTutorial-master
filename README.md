810bdb83c75fde52d480cdf52c65799c21df721d
BS <BS@LNTP078>
2022-08-11 12:48:10 +0200

    Aggiungere i file di progetto.

---
+++ README.md
@@ -0,0 +1,5 @@
+#Xamarin Android YouTube Tutorial
+
+This files is attached to online Xamarin Tutorial on Youtube
+here
+[Xaimarin Studio Arabic  دورة](https://www.youtube.com/playlist?list=PLF8OvnCBlEY3qYheTm4gnaoWwBuBf2TUj)
---
+++ XamarinWebservice/WebService/SellingPartal/Classes/Users.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SellingPartal
+{
+    public class Users
+    {
+        public int IsAvailable(string UserName, string Email)
+        {
+            int UserID = 0;
+            try
+            {
+                SqlDataReader reader;
+                using (SqlConnection connection = new SqlConnection(DBConnection.ConnectionString))
+                {    
+                    SqlCommand cmd = new SqlCommand("SELECT UserID FROM Users where UserName=@UserName or Email=@Email ");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = connection;
+                    cmd.Parameters.AddWithValue("@UserName", UserName);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+                    connection.Open();
+                    
+                    reader = cmd.ExecuteReader();
+                   
+                    while (reader.Read())
+                    {
+                        UserID = reader.GetInt32(0);
+
+                    }
+                     
+                    reader.Close();
+                    connection.Close();
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            return UserID;
+        }
+
+      
+    }
+}
